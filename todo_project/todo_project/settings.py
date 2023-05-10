8e9fa021b61a657d39d68b9e23891a436c0cdb71
Tim Crisp <timothycrisp@gmail.com>
2023-05-10 15:33:43 -0500

    create user app and user model, make migrations

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -31,7 +31,8 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['192.168.0.100']
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -42,8 +43,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'todo_app',
-    'corsheaders'
+    'corsheaders',
+    'users'
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -104,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
---
+++ todo_project/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self):
+        return self.email
